3426b4a436419cc5ce153537be149c809fa48077
Tero Hakkarainen <tero.hakkarainen@solita.fi>
2021-03-15 22:52:08 +0200

    Added puzzle-solving integration test.

---
+++ src/test/kotlin/fi/thakki/sudokusolver/PuzzleSolvingTest.kt
@@ -0,0 +1,23 @@
+package fi.thakki.sudokusolver
+
+import assertk.assertThat
+import assertk.assertions.isTrue
+import fi.thakki.sudokusolver.service.analyzer.PuzzleAnalyzer
+import fi.thakki.sudokusolver.util.PuzzleLoader
+import org.junit.jupiter.api.Test
+
+class PuzzleSolvingTest {
+
+    @Test
+    fun `sample puzzles can be solved`() {
+        listOf(
+            "expert.yml",
+            "puzzle.yml",
+            "small.yml"
+        ).forEach { puzzleFile ->
+            val puzzle = PuzzleLoader.newPuzzleFromFile(puzzleFile)
+            PuzzleAnalyzer(puzzle).analyze(Int.MAX_VALUE)
+            assertThat(puzzle.isComplete()).isTrue()
+        }
+    }
+}
